6ec6a8d9fb82c9a33e1e78d233a4af2a66391d20
Tarek(Tanvir Hasan) <tarekhossain0043@gmail.com>
2025-09-26 23:04:14 +0600

    season-2 start for login page

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Layouts from './Layouts'
 import Home from './pages/Home'
+import Login from './pages/Login'
 import '../styles/index.css'
 export default function App() {
   return (
     <div>
       <Layouts>
         <Home />
+        <Login />
       </Layouts>
     </div>
   )

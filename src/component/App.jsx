197e20b076f7072c08e6e58a2bad05afe98341af
Tarek(Tanvir Hasan) <tarekhossain0043@gmail.com>
2025-09-27 22:34:59 +0600

    navBra branch code push

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,15 +1,8 @@
 import React from 'react'
-import Layouts from './Layouts'
-import Home from './pages/Home'
-import Column from './Column'
-import '../styles/index.css'
 export default function App() {
   return (
     <div>
-      <Layouts>
-        <Home />
-        <Column />
-      </Layouts>
+      hello
     </div>
   )
 }
--- src/component/Videos.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Video from '../component/Video';
-export default function Videos() {
-  return (
-      <div className='videos'>
-        <Video />
-      </div>
-  )
-}
